55340b8ee55e0508ab9f1aed0321b9b777728b28
Anubhav Jain <anubhav94@gmail.com>
2014-08-16 20:42:15 -0400

    Renaming to admin config

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var passport        = require('passport');
 var util            = require('util');
 
 var authConfig      = require('./config/auth');
-var userConfig      = require('./config/user');
+var adminConfig      = require('./config/admin');
 var app             = express();
 
 // Connect to mongodb
